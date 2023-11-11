0cd5fcc24234c130844711a35ece48317afd1ed0
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-11-10 20:30:40 -0600

    Update csv_to_sql.py

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
 """ 
 Created on Fri Nov 10 19:41:34 2023
 
 @author: alia
 """
+import pandas as pd
+import glob, os
+from sqlalchemy import create_engine
+
+for file in glob.glob("*.csv"):
+  df = pd.read_csv(file)
+
+  # Create SQLAlchemy engine to connect to MySQL Database.
+  engine = create_engine("mysql
